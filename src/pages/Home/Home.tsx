68c644cc89651b5da2773690facfa93aadf0f8c1
soumitra-quest <soumitra.d@questlabs.biz>
2025-10-06 23:32:50 +0530

    fix build failure

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,9 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import Navbar from "../../common/Navbar";
-import FullPageSections from "../../components/Slider";
-import AIFactory from "./components/AIFactory";
-import Utilities from "./components/Utilities";
-import Mastery from "./components/Mastery";
-import Testimonial from "./components/Testimonial";
-import FAQ from "./components/FAQ";
-import Blog from "./components/Blog";
-import DemoPage from "./components/DemoPage";
-import Footer from "./components/Footer";
 import ScrollSection from "./components/ScrollSection";
 
 const Home = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
+  const [scrollPosition] = useState(0);
   // const scrollContainerRef = useRef<HTMLDivElement>(null);
 
   // const handleScroll = () => {
